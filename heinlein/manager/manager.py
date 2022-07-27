fc98fc3cccabbed0db550ff8fdc425f767786d2d
Patrick Wells <pwells@ucdavis.edu>
2022-07-27 10:00:44 -0700

    Added type hints and docstrings to manager

--- heinlein/manager/manager.py
+++ heinlein/manager/manager.py
@@ -38,17 +38,31 @@ class Manager:
 
 class FileManager(Manager):
 
-    def __init__(self, name, *args, **kwargs):
+    def __init__(self, name: str, *args, **kwargs) -> None:
+        """
+        A file manager manages files on local disk.
+        This is in contrast to data stored in the cloud
+        Or data in a database
+
+        Params:
+
+        name: the name of the dataset
+        """
         super().__init__(name, *args, **kwargs)
         self.setup()
 
-    def setup(self, *args, **kwargs):
+    def setup(self, *args, **kwargs) -> None:
+        """
+        Performs basic setup of the file manager
+        Loads datset if it exists, or prompts
+        user if dataset does not exist.
+        """
         with open(MAIN_DATASET_CONFIG, "r") as f:
             surveys = json.load(f)
         if self.name not in surveys.keys():
             write_new = self.warning_prompt_tf(f"Survey {self.name} not found, would you like to initialize it? ")
             if write_new:
-                self.config_location = self.initialize_survey()
+                self.config_location = self.initialize_dataset()
             else:
                 self.ready = False
         else:
@@ -57,6 +71,21 @@ class FileManager(Manager):
             self.ready = True
     
     def add_data(self, dtype: str, path: pathlib.Path) -> bool:
+        """
+        Add data to a datset. Note that this only gives the manager
+        a path to the data. The manager itself does not know what kind
+        of data it is or how to use it. Usually this will be invoked
+        by a command line script.
+
+        Params:
+
+        dtype <str>: Type of data being added (i.e. "catalog")
+        path <pathlib.Path>: Path to the data
+
+        Returns:
+
+        bool: Whether or not the file was sucessfully added
+        """
         if not self.ready:
             return False
         with open(self.config_location, "r") as f:
@@ -81,7 +110,16 @@ class FileManager(Manager):
             json.dump(config_data, f, indent=4)
         return True
 
-    def initialize_survey(self, *args, **kwargs):
+    def initialize_dataset(self, *args, **kwargs) -> pathlib.Path:
+        """
+        Initialize a new dataset by name.
+        Creates a default configuration file.
+
+        Returns:
+
+        pathlib.Path: The path to the new configuration file.
+        """
+
         default_survey_config_location = DATASET_CONFIG_DIR / "default.json"
         with open(default_survey_config_location, "r") as f:
             default_survey_config = json.load(f)
@@ -99,4 +137,5 @@ class FileManager(Manager):
             data.update({self.name: {'config_path': f"{self.name}.json"}})
             json.dump(data, f, indent=4)
         
+        self.ready = True
         return output_location
