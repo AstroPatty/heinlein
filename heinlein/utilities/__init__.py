922e6e17379538d6a81d81a009e9e2c332c46290
Patrick Wells <pwells@ucdavis.edu>
2022-08-02 13:50:35 -0700

    Added a "split" utility
    Splits datasets to make use of heinlein's native regions
    functionality

--- heinlein/utilities/__init__.py
+++ heinlein/utilities/__init__.py
@@ -1,4 +1,4 @@
-from .utilities import warning_prompt, warning_prompt_tf
+from .utilities import warning_prompt, warning_prompt_tf, split
 
 
-__all__ = ["warning_prompt", "warning_prompt_tf"]
+__all__ = ["warning_prompt", "warning_prompt_tf", "split"]
