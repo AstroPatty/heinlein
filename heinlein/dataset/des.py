a932edfcab8efac057fb1b1114c3ea9cb2629c96
Patrick Wells <pwells@ucdavis.edu>
2023-08-23 10:21:27 -0700

    Linting on dataset module

--- heinlein/dataset/des.py
+++ heinlein/dataset/des.py
@@ -1,30 +1,33 @@
+import pickle
+import re
+
+import numpy as np
+import pandas as pd
+import pymangle
+from astropy.io import fits
 from heinlein import Region
 from heinlein.dtypes.handlers.handler import Handler
 from heinlein.dtypes.mask import Mask
 from heinlein.locations import BASE_DATASET_CONFIG_DIR
-import numpy as np
-import re
-from pathlib import Path
-import pandas as pd
 from spherical_geometry.polygon import SingleSphericalPolygon
-import pickle
-import pymangle
-from astropy.io import fits
 
 EXPORT = ["load_regions"]
+
 
 def setup(self, *args, **kwargs):
     self._regions = list(load_regions().values())
 
+
 def load_regions():
-    support_location = BASE_DATASET_CONFIG_DIR/ "support"
+    support_location = BASE_DATASET_CONFIG_DIR / "support"
     pickled_path = support_location / "des_tiles.reg"
     if pickled_path.exists():
-        with open(pickled_path, 'rb') as f:
+        with open(pickled_path, "rb") as f:
             regions = pickle.load(f)
     else:
         regions = load_regions_from_pandas(support_location)
     return regions
+
 
 def load_regions_from_pandas(support_location):
     tile_file = support_location / "des_tiles.csv"
@@ -33,41 +36,52 @@ def load_regions_from_pandas(support_location):
     for index, row in tile_data.iterrows():
         ra_par = "RAC"
         dec_par = "DECC"
-        corners = []
         ras = np.zeros(4)
         decs = np.zeros(4)
         for c in range(1, 5):
             ra = row[f"{ra_par}{c}"]
             dec = row[f"{dec_par}{c}"]
-            ras[c-1] = ra
-            decs[c-1] = dec
+            ras[c - 1] = ra
+            decs[c - 1] = dec
 
-        center_point = (row['RA_CENT'], row['DEC_CENT'])
+        center_point = (row["RA_CENT"], row["DEC_CENT"])
         geo = SingleSphericalPolygon.from_radec(ras, decs, center=center_point)
-        reg = Region(geo, name=row['TILENAME'])
-        tiles.update({row['TILENAME']: reg})
-    return tiles    
+        reg = Region(geo, name=row["TILENAME"])
+        tiles.update({row["TILENAME"]: reg})
+    return tiles
 
 
 class MaskHandler(Handler):
     def __init__(self, *args, **kwargs):
         kwargs.update({"type": "mask"})
         super().__init__(*args, **kwargs)
-        self.mangle_files = [f for f in (self._path / "mangle").glob("*.pol") if not f.name.startswith(".")]
-        self.plane_files = [f for f in (self._path / "plane").glob("*.fits") if not f.name.startswith(".")]
-    
+        self.mangle_files = [
+            f
+            for f in (self._path / "mangle").glob("*.pol")
+            if not f.name.startswith(".")
+        ]
+        self.plane_files = [
+            f
+            for f in (self._path / "plane").glob("*.fits")
+            if not f.name.startswith(".")
+        ]
+
     def get_data(self, region_names, *args, **kwargs):
-        nreg = len(region_names)
+        len(region_names)
         regex = re.compile("|".join(region_names))
-        mangle_matches = list(filter(lambda x, y=regex: regex.match(x.name), self.mangle_files))
-        plane_matches = list(filter(lambda x, y=regex: regex.match(x.name), self.plane_files))
+        mangle_matches = list(
+            filter(lambda x, y=regex: regex.match(x.name), self.mangle_files)
+        )
+        plane_matches = list(
+            filter(lambda x, y=regex: regex.match(x.name), self.plane_files)
+        )
         return self._get(region_names, mangle_matches, plane_matches, *args, **kwargs)
 
     def _get(self, regions, mangle_files, plane_files, *args, **kwargs):
         output = {}
         for region in regions:
-            mangle_file = list(filter(lambda x, y=region: y in x.name,mangle_files ))
-            plane_file = list(filter(lambda x, y=region: y in x.name,plane_files ))
+            mangle_file = list(filter(lambda x, y=region: y in x.name, mangle_files))
+            plane_file = list(filter(lambda x, y=region: y in x.name, plane_files))
             bad = False
             if len(mangle_file) == 0:
                 print(f"Unable to find Mangle mask for region {region}")
@@ -80,9 +94,11 @@ class MaskHandler(Handler):
 
             mangle_msk = pymangle.Mangle(str(mangle_file[0]))
             plane_msk = fits.open(plane_file[0], memmap=True)
-            output.update({region: Mask([mangle_msk, plane_msk], pixarray=True, **self._config)})
+            output.update(
+                {region: Mask([mangle_msk, plane_msk], pixarray=True, **self._config)}
+            )
         return output
-    
+
     def get_data_object(self, data, *args, **kwargs):
         masks = list(data.values())
         return masks[0].append(masks[1:])
--- heinlein/dataset/hsc.py
+++ heinlein/dataset/hsc.py
@@ -1,29 +1,28 @@
-from pathlib import Path
-import numpy as np
-from heinlein.dtypes.mask import Mask
-from heinlein.locations import BASE_DATASET_CONFIG_DIR
-from spherical_geometry.polygon import SingleSphericalPolygon
-from heinlein.region import Region
-from heinlein.dtypes.handlers.handler import Handler
-import regions as reg
-import re
 import math
 import operator
 import pickle
+import re
+
+import numpy as np
 import regions as reg
-from shapely import geometry
-from shapely import affinity
-import astropy.units as u
+from heinlein.dtypes.handlers.handler import Handler
+from heinlein.dtypes.mask import Mask
+from heinlein.locations import BASE_DATASET_CONFIG_DIR
+from heinlein.region import Region
+from shapely import affinity, geometry
+from spherical_geometry.polygon import SingleSphericalPolygon
+
 
 def setup(self, *args, **kwargs):
-    reg = load_regions()
-    self._regions = reg
+    _regions = load_regions()
+    self._regions = _regions
+
 
 def load_regions():
-    support_location = BASE_DATASET_CONFIG_DIR  / "support"
+    support_location = BASE_DATASET_CONFIG_DIR / "support"
     pickled_path = support_location / "hsc_regions.reg"
     if pickled_path.exists():
-        with open(pickled_path, 'rb') as f:
+        with open(pickled_path, "rb") as f:
             regions = pickle.load(f)
             return regions
 
@@ -31,7 +30,8 @@ def load_regions():
     files = [f for f in support_location.glob("*.txt") if not f.name.startswith(".")]
     regions = _load_region_data(files=files)
     return regions
-    
+
+
 def _load_region_data(files, *args, **kwargs):
     """
     Loads data about the tracts and patches for the given HSC field
@@ -51,7 +51,7 @@ def _parse_tractfile(tractfile):
     tracts = {}
     with open(tractfile) as tf:
         for line in tf:
-            if line.startswith('*'):
+            if line.startswith("*"):
                 continue
 
             nums = re.findall(r"[-+]?\d*\.\d+|\d+", line)
@@ -59,45 +59,58 @@ def _parse_tractfile(tractfile):
             patch = re.search("Patch", line)
             if patch is None:
                 if tract_num not in tracts.keys():
-                    tracts.update({tract_num: {'corners': [], 'type': 'tract', 'subregions': {} } } )
-                if 'Corner' in line:
-                    tracts[tract_num]['corners'].append((float(nums[-2]), float(nums[-1])))
-                elif 'Center' in line:
-                    tracts[tract_num].update({'center': (float(nums[-2]), float(nums[-1]))})
+                    tracts.update(
+                        {tract_num: {"corners": [], "type": "tract", "subregions": {}}}
+                    )
+                if "Corner" in line:
+                    tracts[tract_num]["corners"].append(
+                        (float(nums[-2]), float(nums[-1]))
+                    )
+                elif "Center" in line:
+                    tracts[tract_num].update(
+                        {"center": (float(nums[-2]), float(nums[-1]))}
+                    )
 
             else:
-                patch = re.findall(r'\d,\d', line)
-                patch_val = tuple(map(int, patch[0].split(',')))
-                if patch_val not in tracts[tract_num]['subregions'].keys():
-                    tracts[tract_num]['subregions'].update({patch_val: {'corners': [], 'type': 'patch'}})
-                if 'Corner' in line:
-                    tracts[tract_num]['subregions'][patch_val]['corners'].append((float(nums[-2]), float(nums[-1])))
-                elif 'Center' in line:
-                    tracts[tract_num]['subregions'][patch_val].update({'center': (float(nums[-2]), float(nums[-1]))})
+                patch = re.findall(r"\d,\d", line)
+                patch_val = tuple(map(int, patch[0].split(",")))
+                if patch_val not in tracts[tract_num]["subregions"].keys():
+                    tracts[tract_num]["subregions"].update(
+                        {patch_val: {"corners": [], "type": "patch"}}
+                    )
+                if "Corner" in line:
+                    tracts[tract_num]["subregions"][patch_val]["corners"].append(
+                        (float(nums[-2]), float(nums[-1]))
+                    )
+                elif "Center" in line:
+                    tracts[tract_num]["subregions"][patch_val].update(
+                        {"center": (float(nums[-2]), float(nums[-1]))}
+                    )
     return tracts
+
 
 def _parse_tractdata(tractdata, *args, **kwargs):
     output = {}
     try:
-        wanted_tracts = kwargs['tracts']
-    except:
+        wanted_tracts = kwargs["tracts"]
+    except KeyError:
         wanted_tracts = []
     for index, (name, tract) in enumerate(tractdata.items()):
         if wanted_tracts and name not in wanted_tracts:
-                continue
-        corners = tract['corners']
-        center = tract['center']
+            continue
+        corners = tract["corners"]
+        center = tract["center"]
 
         points = _parse_polygon_corners(center, corners)
         tract_ra = [p[0] for p in points]
         tract_dec = [p[1] for p in points]
         poly = SingleSphericalPolygon.from_radec(tract_ra, tract_dec, center)
-        region_obj = Region(poly, name=name)
+        Region(poly, name=name)
 
         patches = {}
-        for patchname, patch in tract['subregions'].items():
-            patch_corners = patch['corners']
-            patch_center = patch['center']
+        for patchname, patch in tract["subregions"].items():
+            patch_corners = patch["corners"]
+            patch_center = patch["center"]
             patch_name_parsed = _patch_tuple_to_int(patchname)
             ras = [p[0] for p in patch_corners]
             decs = [p[1] for p in patch_corners]
@@ -105,21 +118,30 @@ def _parse_tractdata(tractdata, *args, **kwargs):
 
             if final_name == "10049.402":
                 import pickle
+
                 with open("bad_points.p", "wb") as f:
-                    out = {'points': points, 'center': patch_center}
+                    out = {"points": points, "center": patch_center}
                     pickle.dump(out, f)
 
             patch_poly = SingleSphericalPolygon.from_radec(ras, decs, patch_center)
             patches.update({final_name: Region(patch_poly, name=final_name)})
-        
+
         output.update(patches)
     return output
 
-def _parse_polygon_corners(center, points):
-    sorted_coords = sorted(points, key=lambda coord: (-135 - math.degrees(math.atan2(*tuple(map(operator.sub, coord, center))[::-1]))) % 360)
 
-    #This ensures the points are ordered counter-clockwsie, to avoid twisted polygons
-    #Shoutout to StackOverflow
+def _parse_polygon_corners(center, points):
+    sorted_coords = sorted(
+        points,
+        key=lambda coord: (
+            -135
+            - math.degrees(math.atan2(*tuple(map(operator.sub, coord, center))[::-1]))
+        )
+        % 360,
+    )
+
+    # This ensures the points are ordered counter-clockwsie, to avoid twisted polygons
+    # Shoutout to StackOverflow
     return sorted_coords
 
 
@@ -131,16 +153,17 @@ def _patch_tuple_to_int(patch_tuple):
     if patch_tuple[0] == 0:
         return patch_tuple[1]
     else:
-        return 100*patch_tuple[0] + patch_tuple[1]
+        return 100 * patch_tuple[0] + patch_tuple[1]
+
 
 def _patch_int_to_tuple(patch_int):
     if patch_int < 0:
         return (0, patch_int)
     else:
-        return (patch_int // 100, patch_int %100)
+        return (patch_int // 100, patch_int % 100)
+
 
 class MaskHandler(Handler):
-
     def __init__(self, *args, **kwargs):
         kwargs.update({"type": "mask"})
         super().__init__(*args, **kwargs)
@@ -158,10 +181,8 @@ class MaskHandler(Handler):
             mask = reg.Regions.read(str(path))
             new_masks = np.empty(len(mask), dtype=object)
 
-            for i,r in enumerate(mask):
+            for i, r in enumerate(mask):
                 if type(r) == reg.CircleSkyRegion:
-        
-
                     new_reg = Region.circle(r.center, r.radius.to_value("deg"))
                 elif type(r) == reg.RectangleSkyRegion:
                     center = r.center
@@ -170,11 +191,15 @@ class MaskHandler(Handler):
                     width = r.width.to_value("deg")
                     height = r.height.to_value("deg")
                     angle = r.angle.to_value("deg")
-                    box = geometry.box(x - width/2, y - height/2, x + width/2, y + height/2)
+                    box = geometry.box(
+                        x - width / 2, y - height / 2, x + width / 2, y + height / 2
+                    )
                     new_reg = affinity.rotate(box, angle)
                     x_coords, y_coords = new_reg.exterior.xy
-                    inside = (x,y)
-                    new_reg = SingleSphericalPolygon.from_lonlat(x_coords, y_coords, center = inside)
+                    inside = (x, y)
+                    new_reg = SingleSphericalPolygon.from_lonlat(
+                        x_coords, y_coords, center=inside
+                    )
                     new_reg = Region.polygon(new_reg)
 
                 new_masks[i] = new_reg
@@ -188,4 +213,3 @@ class MaskHandler(Handler):
     def get_data_object(self, objs):
         objs_ = list(objs.values())
         return objs_[0].append(objs_[1:])
-        
