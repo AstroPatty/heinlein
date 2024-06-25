b6056434e5d7e1518436ec47b9d4b3bf233270eb
Patrick Wells <patrick.wells.95@gmail.com>
2024-06-25 14:00:10 -0700

    Move some extension logic

--- heinlein/dataset/__init__.py
+++ heinlein/dataset/__init__.py
@@ -1,3 +1,4 @@
-from .dataset import Dataset, dataset_extension, load_current_config, load_dataset
+from .dataset import Dataset, load_current_config, load_dataset
+from .extension import dataset_extension
 
 __all__ = ["Dataset", "load_dataset", "load_current_config", "dataset_extension"]
