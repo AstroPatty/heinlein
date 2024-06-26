b76e4343bc5c8cc30148a2ed7b04340a51596dd5
Patrick Wells <patrick.wells.95@gmail.com>
2024-06-26 12:36:49 -0700

    Update top-level imports

--- heinlein/__init__.py
+++ heinlein/__init__.py
@@ -1,8 +1,9 @@
 from heinlein import api
 
+from .config import get_option, set_option
 from .dataset import load_dataset
 from .region import Region
 
 __version__ = "0.9.1"
-__all__ = ["load_dataset", "Region"]
+__all__ = ["load_dataset", "get_option", "set_option", "Region"]
 __all__.extend(api.__all__)
--- heinlein/config/__init__.py
+++ heinlein/config/__init__.py
@@ -2,13 +2,13 @@ from typing import Any
 
 from .config import HeinleinConfig
 
-HEINLEIN_CONFIG = HeinleinConfig()
+_HEINLEIN_CONFIG = HeinleinConfig()
 
 
-def get_config(option: str) -> Any:
-    return getattr(HEINLEIN_CONFIG, option)
+def get_option(option: str) -> Any:
+    return getattr(_HEINLEIN_CONFIG, option)
 
 
-def set_config(option: str, value: Any) -> bool:
-    setattr(HEINLEIN_CONFIG, option, value)
+def set_option(option: str, value: Any) -> bool:
+    setattr(_HEINLEIN_CONFIG, option, value)
     return True
