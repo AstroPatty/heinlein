566011055bc97ada2433c759a8aee948c2d29142
Patrick Wells <patrick.wells.95@gmail.com>
2024-06-26 16:11:59 -0700

    Fix get command

--- heinlein/cmds.py
+++ heinlein/cmds.py
@@ -1,6 +1,4 @@
 import click
-from godata import load_project
-from godata.project import GodataProjectError
 
 from heinlein import api
 
@@ -29,18 +27,9 @@ def remove(dataset_name, data_type):
 @click.command()
 @click.argument("dataset_name")
 @click.argument("data_type")
-def get(dataset_name, data_type) -> bool:
+def get(dataset_name, data_type) -> str:
     """
     Get the path to a specific data type in a specific datset
     """
-    try:
-        project = load_project(dataset_name, ".heinlein")
-    except GodataProjectError:
-        print(f"Error: dataset {dataset_name} does not exist!")
-        return False
-    try:
-        path = project.get("data/" + data_type, as_path=True)
-    except GodataProjectError:
-        print(f"Error: data type {data_type} does not exist in dataset {dataset_name}")
-        return False
-    click.echo(path)
+    path = api.get(dataset_name, data_type)
+    return str(path)
