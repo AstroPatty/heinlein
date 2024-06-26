d0c5c894f82351330412c47e03f06c8e28164bff
Patrick Wells <patrick.wells.95@gmail.com>
2024-06-26 13:12:13 -0700

    QOL Updates to options

--- heinlein/errors.py
+++ heinlein/errors.py
@@ -1,2 +1,8 @@
 class HeinleinError(Exception):
     pass
+
+
+class HeinleinConfigError(HeinleinError):
+    @classmethod
+    def doesnt_exist(cls, option: str) -> "HeinleinConfigError":
+        return cls(f"'{option}' is not a valid configuration option")
