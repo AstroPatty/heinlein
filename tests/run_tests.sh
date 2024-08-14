64f5543669961f22ad10336016d04a90698eafa3
Patrick Wells <patrick.wells.95@gmail.com>
2024-08-14 16:43:41 -0500

    Smarter testing script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cd tests && poetry run pytest 
+# Get the TEST_SET environment variable
+if [ -z "$TEST_SET" ]; then
+  TEST_SET=""
+else
+  TEST_SET="test_$TEST_SET.py"
+fi
+cd tests && poetry run pytest $TEST_SET
