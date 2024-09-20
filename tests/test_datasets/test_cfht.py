b95633e8d1befc818ea29fefa15a65174436d924
Patrick Wells <patrick.wells.95@gmail.com>
2024-09-20 14:44:42 -0500

    move some tests

---
+++ tests/test_datasets/test_cfht.py
@@ -0,0 +1,67 @@
+import pickle
+from pathlib import Path
+
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from pytest import fixture
+
+from heinlein import load_dataset, set_option
+from heinlein.api import add
+from heinlein.region.footprint import Footprint
+
+DATA_PATH = Path("/home/data")
+radius = 2 * u.arcmin
+
+
+def setup_module():
+    add_cfht()
+
+
+@fixture
+def dataset():
+    set_option("CACHE_SIZE", "10G")
+    return load_dataset("cfht")
+
+
+@fixture
+def center():
+    regions_path = DATA_PATH / "cfht" / "test_regions.reg"
+    with open(regions_path, "rb") as f:
+        regions = pickle.load(f)
+    regions = {r.name: r for r in regions}
+    f = Footprint(regions)
+    center = f._footprint.centroid
+    center = SkyCoord(center.x, center.y, unit="deg")
+    return center
+
+
+def add_cfht():
+    catalog_path = DATA_PATH / "cfht" / "cfht_test.db"
+    mask_path = DATA_PATH / "cfht" / "mask"
+    add("cfht", "catalog", str(catalog_path), force=True)
+    add("cfht", "mask", str(mask_path), force=True)
+
+
+def test_cfht_cone_search(dataset, center):
+    second_center = center.directional_offset_by(0, 4 * u.arcmin)
+    a = d.cone_search(center, radius, dtypes=["catalog", "mask"])
+    cat = a["catalog"]
+    mask = a["mask"]
+    masked_cat = mask.mask(cat)
+    assert len(cat) >= len(masked_cat)
+    assert len(cat) > 0
+
+    a = d.cone_search(second_center, radius, dtypes=["catalog", "mask"])
+    cat = a["catalog"]
+    mask = a["mask"]
+    masked_cat = mask.mask(cat)
+    assert len(cat) >= len(masked_cat)
+    assert len(cat) > 0
+
+
+def test_cfht_box_search(dataset, center):
+    data = dataset.box_search(center, 2 * radius, dtypes=["catalog", "mask"])
+    cat = data["catalog"]
+    mask = data["mask"]
+    masked_cat = mask.mask(cat)
+    assert len(cat) >= len(masked_cat)
---
+++ tests/test_datasets/test_des.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from pytest import fixture
+
+import heinlein
+from heinlein import load_dataset
+from heinlein.api import add
+
+radius = 2 * u.arcmin
+
+DATA_PATH = Path("/home/data")
+
+DES_CENTER = SkyCoord(7.8, -33.9, unit="deg")
+
+
+def setup_module():
+    add_des()
+
+
+@fixture
+def dataset():
+    heinlein.set_option("CACHE_SIZE", "10G")
+    return load_dataset("des")
+
+
+def add_des():
+    DES_DATA_PATH = DATA_PATH / "des"
+    catalog_path = DES_DATA_PATH / "catalog" / "des_test.db"
+    mask_path = DES_DATA_PATH / "mask"
+    add("des", "catalog", str(catalog_path), force=True)
+    add("des", "mask", str(mask_path), force=True)
+
+
+def test_des_cone_search(dataset):
+    dataset = load_dataset("des")
+    a = dataset.cone_search(DES_CENTER, radius, dtypes=["catalog", "mask"])
+    cat = a["catalog"]
+    mask = a["mask"]
+    masked_cat = mask.mask(cat)
+    assert len(cat) >= len(masked_cat)
+
+    new_center = DES_CENTER.directional_offset_by(0, 4 * u.arcmin)
+    a = dataset.cone_search(new_center, radius, dtypes=["catalog", "mask"])
+    cat = a["catalog"]
+    mask = a["mask"]
+    masked_cat = mask.mask(cat)
+    assert len(cat) >= len(masked_cat)
+
+
+def test_des_box_search(dataset):
+    a = dataset.box_search(DES_CENTER, radius, dtypes=["catalog", "mask"])
+    cat = a["catalog"]
+    mask = a["mask"]
+    masked_cat = mask.mask(cat)
+    assert len(cat) >= len(masked_cat)
