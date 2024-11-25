Fix some masking issues

datasets/cfht/heinlein_cfht/cfht.py
@@ -45,11 +45,11 @@ class MaskHandler(Handler):
                 continue
 
             path = matches[0]
-            data = fits.open(path)
-            out = np.empty(1, dtype="object")
-            out[0] = data
-            mask_obj = mask.Mask(out, pixarray=True, **self._config)
-            output.update({n: mask_obj for n in regions_[name]})
+            with fits.open(path) as data:
+                out = np.empty(1, dtype="object")
+                out[0] = data
+                mask_obj = mask.Mask(out, pixarray=True, **self._config)
+                output.update({n: mask_obj for n in regions_[name]})
         return output
 
     def get_data_object(self, data, *args, **kwargs):

heinlein/dtypes/handlers/mask.py
@@ -33,11 +33,11 @@ class FitsMaskHandler(Handler):
             if len(matches) > 1:
                 logging.error(f"Error: Found more than one mask for region {name}")
                 continue
-
-            data = fits.open(matches[0])
-            out = np.empty(1, dtype="object")
-            out[0] = data
-            output.update({name: out})
+            file = matches[0]
+            with fits.open(file) as data:
+                out = np.empty(1, dtype="object")
+                out[0] = data
+                output.update({name: out})
 
         return output
 